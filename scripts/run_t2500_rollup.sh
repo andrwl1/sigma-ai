4b9af6c789e330370addaf606f384cceb8fa137b
andrwl1 <andrwl1@users.noreply.github.com>
2025-10-22 20:38:03 -0400

    fix(t2500_rollup): use sigma_ai.eval.rollup module path

--- scripts/run_t2500_rollup.sh
+++ scripts/run_t2500_rollup.sh
@@ -3,7 +3,7 @@ set -e
 input_dir="$1"
 history="$2"
 
-python -m sigma.eval.rollup \
+python -m sigma_ai.eval.rollup \
   --metrics "$input_dir/metrics.json" \
   --history "$history" \
   --label "t2500_nightly"
